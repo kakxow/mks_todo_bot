5690483f9b6bb905602fed24c5f35c4934dd2d78
Maksim Sukhanov <kakxow@gmail.co>
2023-05-02 15:46:04 +0400

    Fixed markdown errors

--- commands.py
+++ commands.py
@@ -1,6 +1,7 @@
 import telegram
 from telegram.ext import MessageHandler, Filters
 from telegram.ext.callbackqueryhandler import CallbackQueryHandler
+from telegram.helpers import escape_markdown
 
 
 inline_keyboard_markup_mark_complete = {
@@ -28,41 +29,43 @@ inline_keyboard_markup_unmark_complete = {
 def _mark_complete(update, context):
     text = update.effective_message.text
     if text:
-        new_text = f"~{text}~"
+        new_text = escape_markdown(f"~{text}~", version=2)
         context.bot.editMessageText(
             chat_id=update.effective_chat.id,
             message_id=update.effective_message.message_id,
             text=new_text,
             parse_mode=telegram.constants.PARSEMODE_MARKDOWN_V2,
-            reply_markup=inline_keyboard_markup_unmark_complete
+            reply_markup=inline_keyboard_markup_unmark_complete,
         )
     else:
         context.bot.editMessageReplyMarkup(
             chat_id=update.effective_chat.id,
             message_id=update.effective_message.message_id,
-            reply_markup=inline_keyboard_markup_unmark_complete
+            reply_markup=inline_keyboard_markup_unmark_complete,
         )
 
 
 def _unmark_complete(update, context):
     text = update.effective_message.text
     if text:
-        new_text = text.strip("~")
+        new_text = escape_markdown(text.strip("~"), version=2)
         context.bot.editMessageText(
             chat_id=update.effective_chat.id,
             message_id=update.effective_message.message_id,
             text=new_text,
             parse_mode=telegram.constants.PARSEMODE_MARKDOWN_V2,
-            reply_markup=inline_keyboard_markup_mark_complete
+            reply_markup=inline_keyboard_markup_mark_complete,
         )
     else:
         context.bot.editMessageReplyMarkup(
             chat_id=update.effective_chat.id,
             message_id=update.effective_message.message_id,
-            reply_markup=inline_keyboard_markup_mark_complete
+            reply_markup=inline_keyboard_markup_mark_complete,
         )
 
 
-start = MessageHandler(Filters.chat_type.channel & (~Filters.update.edited_channel_post), _unmark_complete)
+start = MessageHandler(
+    Filters.chat_type.channel & (~Filters.update.edited_channel_post), _unmark_complete
+)
 mark_complete = CallbackQueryHandler(_mark_complete, pattern="complete")
 unmark_complete = CallbackQueryHandler(_unmark_complete, pattern="not_complete")
